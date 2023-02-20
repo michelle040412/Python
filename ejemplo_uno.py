24f5d571e95b679320b6abae7dac50ee0b59e92c
michelle040412 <sara.miche01@gmail.com>
2023-02-20 07:35:25 -0500

    20/02

---
+++ ejemplo_uno.py
@@ -0,0 +1,20 @@
+"""
+Variable = 5
+variable_uno= float(variable)
+variable_dos=str(variable)
+
+sumar = variable_uno + variable_dos
+
+#print(sumar)
+
+# print(variable , variable_uno , variable_dos)
+
+#print(type(variable_dos))
+"""
+
+#TIPO DE DATOS
+VARIABLE = "HOLA"
+VARIABLE2 = 20
+VARIABLE3 =20.8
+
+print(VARIABLE3)
